28d6bd377e01ff34d4cc2251a761e33a951ae05e
FILIPE MOTERANI FULANETO <11314975625@dc01.ifs.ifsuldeminas.edu.br>
2026-03-16 07:51:14 -0300

    codigo condicional

---
+++ aula2/Condicional.py
@@ -0,0 +1,12 @@
+# IF - Estrutura Condicional
+
+#Variavel  + Conversão de Str em real (float)+ Entrada
+media= float(input("Digite a média do aluno: "))
+
+#Processamento
+if(media>6):
+    #caminho do sim
+    print("Você está aprovado")
+else:
+    #caminho do não
+    print("Você está reprovado")
