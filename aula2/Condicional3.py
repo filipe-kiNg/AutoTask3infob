db3eb76b5e92338dba38af80b689c30b72aeaee7
FILIPE MOTERANI FULANETO <11314975625@dc01.ifs.ifsuldeminas.edu.br>
2026-03-16 08:16:45 -0300

    novos arquivos

---
+++ aula2/Condicional3.py
@@ -0,0 +1,12 @@
+#Estrutura Condicional elif
+
+
+#entrada
+media = float(input("Digite sua média: "))
+
+if media > 6:
+    print("Aprovado")
+elif media > 3:
+    print("Exame final")
+else:
+    print("Reprovado")
