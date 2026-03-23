cfed3217ec9118e49d54809690cce86c35947e1b
FILIPE MOTERANI FULANETO <11314975625@dc01.ifs.ifsuldeminas.edu.br>
2026-03-23 07:50:40 -0300

    for explicacao

---
+++ aula3/for1.py
@@ -0,0 +1,19 @@
+#Estrutura de repetição For: Utilizado para repetir um conjunto
+#Instrução - comandos, quando sabemos quantas vezes repetir
+
+#Exemplo 1
+
+#range(1,10,1): Intervalo gerado --> 1,2,3,4,5,6,7,8,9
+
+for i in range(1,10,1):
+    print(i, end=" ")    
+
+print("\n")
+
+for i in range(1,7,2):
+    print(i, end=" ")
+
+print("\n")
+
+for i in range(10):
+    print(i, end=" ")
