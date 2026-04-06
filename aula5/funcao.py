f41660235395253203af3b88b44866568658bcbc
FILIPE MOTERANI FULANETO <11314975625@dc01.ifs.ifsuldeminas.edu.br>
2026-04-06 07:37:41 -0300

    Aula 5: import from

---
+++ aula5/funcao.py
@@ -0,0 +1,25 @@
+
+def imprimir(texto):
+    print(texto)
+
+def pulalinha():
+    print("\n")
+
+def ler():
+    return int(input())
+
+def somar(a,b):
+    return a + b
+'''
+imprimir("Receba")
+a = ler()
+
+imprimir("receba 2")
+b = ler()
+
+
+
+
+resposta = somar(a,b)
+imprimir(f"O resultado é {resposta}")
+'''
---
+++ aula5/usarfunc.py
@@ -0,0 +1,15 @@
+from funcao import somar, imprimir, pulalinha, ler
+
+imprimir("Digite o valor de a: ")
+a = ler()
+
+pulalinha()
+
+imprimir("Digite o valor de b: ")
+b = ler()
+
+imprimir(somar(a,b))
+
+
+
+
