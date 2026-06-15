3911e509a399348419624253641e390024029caa
FILIPE MOTERANI FULANETO <11314975625@dc01.ifs.ifsuldeminas.edu.br>
2026-06-15 08:40:59 -0300

    aula 10

---
+++ pandas/atividade.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+sheet = pd.read_excel("pandas\\notas_estudantes.xlsx")
+
+df_notas = pd.read_excel("pandas\\notas_estudantes.xlsx", sheet_name="Notas")
+df_atividades = pd.read_excel("pandas\\notas_estudantes.xlsx", sheet_name="Atividades")
+
+df_notas.loc[len(df_notas)] = ["Lucas silva", "Prova Final", 8.5]
+
+df_notas.loc[(df_notas["Nome"] == "Ana Souza") & (df_notas["Atividade"] == "Trabalho 1"), "Nota"] = 9
+
+df_notas.drop(df_notas[(df_notas["Nome"] == "Pedro Santos") & (df_notas["Atividade"] == "Prova 1")].index, inplace=True)
+print(df_notas)
+media = ["nome", "media"]
+try:
+    for nota in df_notas["Nota"]:
+        if nota >= 7:
+            print(nota)
+except:
+    print("acabou numero")
+
+media = df_notas.groupby("Nome")["Nota"].mean()
+
+print(df_notas[["Nome","Nota"]])
+        
+
+
+
+
