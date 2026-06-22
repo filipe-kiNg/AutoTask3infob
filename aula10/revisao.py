37b6579944f64a9a72de53acae5b49ddc7270c61
filipe-kiNg <k7595743@gmail.com>
2026-06-22 08:44:58 -0300

    Revisao

--- aula10/revisao.py
+++ aula10/revisao.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 sheet = pd.read_excel("aula10\\planilha.xlsx")
 #Nome, Sexo, Idade, Curso, Disciplina, Nota
-sheet.loc[len(sheet)] = ["Pedro Alvarenga", "Masculino", 20, "Engenharia", "Calculo", 5]
-print(sheet.head())
+sheet.loc[len(sheet)] = ["Pedro Alvarenga", "Masculino", 20, "Engenharia", "Calculo", 8]
 
 cond = (sheet["Nome"] == "Ana Silva")
 sheet.loc[cond, "Nota"] = 10
@@ -12,5 +11,7 @@ sheet.loc[cond, "Nota"] = 10
 cond2 = (sheet["Nota"] < 7)
 sheet = sheet.loc[~cond2]
 
-sheet = sheet.sort_values("Curso")
+sheet = sheet.sort_values("Nome")
+qtd = sheet.value_counts("Idade")
 print(sheet)
+print(qtd)
