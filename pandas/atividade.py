27bc382fde1e116b50c8f9a7e7aea12d8bba8418
filipe-kiNg <k7595743@gmail.com>
2026-06-21 22:37:56 -0300

    atividades pandas

--- pandas/atividade.py
+++ pandas/atividade.py
@@ -5,25 +5,28 @@ sheet = pd.read_excel("pandas\\notas_estudantes.xlsx")
 df_notas = pd.read_excel("pandas\\notas_estudantes.xlsx", sheet_name="Notas")
 df_atividades = pd.read_excel("pandas\\notas_estudantes.xlsx", sheet_name="Atividades")
 
-df_notas.loc[len(df_notas)] = ["Lucas silva", "Prova Final", 8.5]
+df_notas.loc[len(df_notas)] = ["Lucas Silva", "Prova final", 8.5]
 
-df_notas.loc[(df_notas["Nome"] == "Ana Souza") & (df_notas["Atividade"] == "Trabalho 1"), "Nota"] = 9
+#cond = (df_notas["Nome"] == "Ana Souza") & (df_notas["Atividade"] == "Trabalho 1") 
+#df_notas.loc[cond, "Nota"] = 9
 
-df_notas.drop(df_notas[(df_notas["Nome"] == "Pedro Santos") & (df_notas["Atividade"] == "Prova 1")].index, inplace=True)
-print(df_notas)
-media = ["nome", "media"]
-try:
-    for nota in df_notas["Nota"]:
-        if nota >= 7:
-            print(nota)
-except:
-    print("acabou numero")
+#cond1 = (df_notas["Nome"] == "Pedro Santos") & (df_notas["Atividade"] == "Prova 1") 
+#df_notas = df_notas[~cond1]
 
-media = df_notas.groupby("Nome")["Nota"].mean()
+#f_notas = df_notas.loc[df_notas["Nota"] > 7]
 
-print(df_notas[["Nome","Nota"]])
-        
+#df_notas = df_notas.groupby("Nome")["Nota"].mean()
 
+#print(df_notas[["Nome","Nota"]])
 
+#df_notas = df_notas[df_notas["Atividade"] == "Prova Final"]
 
+#cond = df_notas["Nota"] > 7
+#print(df_notas.loc[cond,["Nome", "Atividade"]])
+
+#df_notas.sort_values("Nome", inplace=True)
+
+#print(df_atividades.merge(df_notas))
+
+df_notas.to_csv("notas_estudantes_ordenado.xlsx", index=False, encoding="utf-8")
 
