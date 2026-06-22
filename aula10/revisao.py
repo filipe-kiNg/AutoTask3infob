956632d799f0dc6f23cf11796f383bcab184a0da
filipe-kiNg <k7595743@gmail.com>
2026-06-22 07:55:40 -0300

    Revisao

---
+++ aula10/revisao.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+sheet = pd.read_excel("aula10\\planilha.xlsx")
+#Nome, Sexo, Idade, Curso, Disciplina, Nota
+sheet.loc[len(sheet)] = ["Pedro Alvarenga", "Masculino", 20, "Engenharia", "Calculo", 5]
+print(sheet.head())
+
+cond = (sheet["Nome"] == "Ana Silva")
+sheet.loc[cond, "Nota"] = 10
+
+
+cond2 = (sheet["Nota"] < 7)
+sheet = sheet.loc[~cond2]
+
+sheet = sheet.sort_values("Curso")
+print(sheet)
