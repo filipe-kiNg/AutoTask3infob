436ac0acd4683e01c67980d85eb3c354bf6e767e
filipe-kiNg <k7595743@gmail.com>
2026-07-05 21:37:07 -0300

    trabalho ivan

---
+++ trabalho/app.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import pyautogui as pya
+from time import sleep
+
+#nao usei ia
+sheet = pd.read_excel("trabalho\\clientes.xlsx")
+
+
+pya.hotkey("win","d")
+pya.press("win")
+sleep(0.5)
+pya.write("notas")
+sleep(0.5)
+pya.press("enter")
+sleep(3)
+pya.hotkey("win","up")
+
+
+for index, row in sheet.iterrows():
+    name = row['Nome']
+    cpf = row['Cpf']
+    prd = row["Produto"]
+    preco = str(row["Preco"])
+
+    pya.write(name)
+    pya.write(" ")
+    pya.write(cpf)
+    pya.write(" ")
+    pya.write(prd)
+    pya.write(" ")
+    pya.write("R$")
+    pya.write(preco)
+    pya.press("enter")
+    
+    
+  
+        
+
+
+
+
+
+
